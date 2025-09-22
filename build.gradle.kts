1cf6767fc9c520c0e28e45465620600b10db9c9a
grubhart <grubhart@gmail.com>
2025-09-22 02:04:57 -0600

    TESIS-180  feat(health): Añadir endpoint de salud con Spring Boot Actuator
               Implementa un endpoint de chequeo de salud en `/actuator/health`
               añadiendo la dependencia de Spring Boot Actuator y exponiendo el
               endpoint a través de la configuración web.
    
               Este es el primer paso para resolver el error `502 Bad Gateway`,
               proporcionando una URL de chequeo de salud fiable para el entorno
               de Elastic Beanstalk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ repositories {
 extra["springModulithVersion"] = "1.4.1"
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation") // Para validar DTOs y otros objetos con anotaciones.
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
