145eee6faeeb696c5cd3650d6e38c755a4f74d1e
grubhart <grubhart@gmail.com>
2025-11-06 13:25:54 -0600

    TESIS-187 fix session persistence errors

--- src/main/java/org/grubhart/pucp/tesis/TesisBachillerBackendMultimodularApplication.java
+++ src/main/java/org/grubhart/pucp/tesis/TesisBachillerBackendMultimodularApplication.java
@@ -3,9 +3,11 @@ package org.grubhart.pucp.tesis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
 @EnableScheduling // Habilita la detección y ejecución de tareas programadas (@Scheduled)
+@EnableJdbcHttpSession // Habilita Spring Session JDBC para sesiones compartidas entre instancias
 public class TesisBachillerBackendMultimodularApplication {
 
     public static void main(String[] args) {
